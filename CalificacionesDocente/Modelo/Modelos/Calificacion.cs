2a4072ae0078bba4c8cf1039a1d753733500cc73
jesus_arango_restrepo <jesus.arango.restrepo@gmail.com>
2022-03-18 08:03:38 -0500

    ajuste modelos

--- CalificacionesDocente/Modelo/Modelos/Calificacion.cs
+++ CalificacionesDocente/Modelo/Modelos/Calificacion.cs
@@ -5,8 +5,8 @@
         public int FormatoId { get; set; }
         public int EvaluacionId { get; set; }
         public int Nota { get; set; }
-        public int Observacion { get; set; }
-        public int PlanMejora { get; set; }
+        public string Observacion { get; set; }
+        public bool? PlanMejora { get; set; }
 
     }
 }
--- CalificacionesDocente/Modelo/Modelos/Evaluacion.cs
+++ CalificacionesDocente/Modelo/Modelos/Evaluacion.cs
@@ -4,6 +4,11 @@ namespace Modelo.Modelos
 {
     public class Evaluacion : CamposControlCrea, CamposControlEdita
     {
+     public  int Id { get; set; }
+        public string Documento { get; set; }
+        public int CursoId { get; set; }
+        public int SemestreId { get; set; }
+        public int Sedeid { get; set; }
         public DateTime FechaMod { get; set; }
         public int UsuarioMod { get; set; }
         public DateTime FechaCrea { get; set; }
