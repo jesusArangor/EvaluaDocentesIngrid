8b357237ebda98f22f91e4e4f8ce49623629ac4d
jesus_arango_restrepo <jesus.arango.restrepo@gmail.com>
2022-03-24 19:37:34 -0500

    ajuste angular

---
+++ CalificacionesDocente/WebApi/Controllers/ApiEvaluaccionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modelo.Interfaces;
+
+namespace WebApi.Controllers
+{
+    public class ApiEvaluaccionController : Controller, IApi
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
